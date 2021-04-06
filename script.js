bbbbaa3f691306246cd4aaef245be410a58aaa3a
Сергей Рожко <serg6600@yandex.ru>
2021-04-06 18:19:40 +0300

    Добавил выводы в консоль, изменил преобразование в строку

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let num = 266219;
-let numStr= num+'';
+let numStr= String(num);
 let total = 1;
 
 for (let i=0; i<numStr.length; i++){
     total = total * numStr[i];
 }
+console.log(total);
 
 total=total*total*total;
-total+='';
+console.log(total);
+
+total = String(total);
 
 console.log(total[0]+total[1]);
