5f3bf6328da10629fbdff3de8468b0f815ccd114
Сергей Рожко <serg6600@yandex.ru>
2021-04-15 22:40:42 +0300

    first

---
+++ script/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let date = new Date();
+let today = date.getDay();
+
+let fixdata = function(today){
+    if (today == 0){
+        today = 6;
+        return(today);
+        }
+    today--;
+    return(today);
+};
+
+for(let item in week){
+    let div = document.createElement('div');
+    if (week[item] === 'Sunday' || week[item] === 'Saturday'){
+    div.className = 'weekend';
+    } else {
+        div.className = 'workday';
+    }
+    if (fixdata(today) == item) {
+        div.classList.add('today');
+    }
+    div.textContent = week[item];
+    document.body.append(div);
+}
